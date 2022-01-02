d794ab2e5779b3e569ac18d09627b075d47f6603
Anrighe <assoonpc@gmail.com>
2022-01-03 00:59:57 +0100

    80% Commenti Inizializzatore

--- Inizializzatore.java
+++ Inizializzatore.java
@@ -57,6 +57,20 @@ public class Inizializzatore
 	@SuppressWarnings("unused")
 	private TableModelListener listenerModifica; 
 	
+	/** Pattern per il contenuto cella di tipo operazione.
+	 * 	<p>Regex utilizzata: <b>^=[A-Z][1-2]{0,1}[0-9][\\+|-][A-Z][1-2]{0,1}[0-9]$</b></p> */ 
+	private Pattern patternOperazioni;
+	
+	/** Matcher utilizzato per confrontare il contenuto della cella con il pattern per operazioni */
+	private Matcher matcherOperazioni;
+	
+	/** Pattern per il contenuto cella di tipo numerico.
+	 * 	<p>Regex utilizzata: <b>^-{0,1}[0-9]+$</b></p> */ 
+	private Pattern patternNumeri;
+	
+	/** Matcher utilizzato per confrontare il contenuto della cella con il pattern per numeri */
+	private Matcher matcherNumeri;
+	
 	/** True se la cella e' di tipo <b>CellaTesto</b> */
 	private boolean ritMatcherTesto = false; 
 	
@@ -69,11 +83,59 @@ public class Inizializzatore
 	/** Monitora che monitora se e' gia' stato creato un thread */
 	private boolean threadCreato = false; 
 	
+	/** Delay in secondi del salvataggio automatico */
+	private final int delaySalvataggio = 0;
+	
 	/** Intervallo di secondi che separa ogni salvataggio automatico */
 	private final int timerAutosalvataggio = 30; 
 	
 	/** Usata per ottenere l'attuale percorso assoluto */
 	private File percorsoCorrente; 
+	
+	/** Contenitore per le opzioni del menu <b>File</b> */ 
+	private JMenuBar barraFile;
+    
+	/** Menu drop-down <b>File</b> */ 
+	private JMenu menuFile;
+	
+    /** Opzione <b>Nuovo</b> del menu <b>File</b> */
+	private JMenuItem opzioneMenuFile1;
+	
+	/** Opzione <b>Apri</b> del menu <b>File</b> */
+	private JMenuItem opzioneMenuFile2;
+	
+	/** Opzione <b>Salva con nome</b> del menu <b>File</b> */
+	private JMenuItem opzioneMenuFile3;
+	
+	/** Opzione <b>Esci</b> del menu <b>File</b> */
+	private JMenuItem opzioneMenuFile4;
+	
+	/** Contenitore per le opzioni del menu <b>Aiuto</b> */ 
+	private JMenuBar barraHelp;
+	
+	/** Menu drop-down <b>Aiuto</b> */ 
+	private JMenu menuHelp;
+	
+	/** Opzione <b>Documentazione</b> del menu <b>Aiuto</b> */
+	private JMenuItem opzioneMenuHelp1;
+	
+	/** Opzione <b>Traccia d'esame</b> del menu <b>Aiuto</b> */
+	private JMenuItem opzioneMenuHelp2;
+	
+	private JPanel pannelloNordUpper;
+	
+	private JPanel pannelloNord;
+    
+	
+	private FlowLayout leftAlignment;
+	
+	
+	private JScrollPane sp;
+	
+	private DefaultTableCellRenderer setPrimaColonna;
+	
+	private Frame finestra;
+	
 	
 	/** Costruttore che inizializza tutte le componenti del foglio elettronico.
 	 * <p>- Istanzia un oggetto per la struttura dati principale [<b>matrice</b>] e per quella secondaria [<b>sottomatrice</b>]</p>
@@ -147,7 +209,11 @@ public class Inizializzatore
 		table = new JTable(dati);
 
 		table.getModel().addTableModelListener(listenerModifica = new TableModelListener()
-		{
+		{	
+			
+			/** Metodo invocato ad ogni modifica del JTable
+			 *  * @param listenerModifica
+			 */
 			public void tableChanged(TableModelEvent listenerModifica) 
 			{
 				System.out.println("Contenuto cella modificato: " + table.getValueAt(table.getSelectedRow(), table.getSelectedColumn())); //debug
@@ -163,45 +229,45 @@ public class Inizializzatore
 				
 				if (!text.equals(""))
 				{
-					Pattern patternOperazioni = Pattern.compile("^=[A-Z][1-2]{0,1}[0-9][\\+|-][A-Z][1-2]{0,1}[0-9]$");
-					Matcher matcherOperazioni = patternOperazioni.matcher(text);
+					/** Controlla se nella cella è contenuta un'addizione o una sottrazione del tipo =A1+A2 */
+					patternOperazioni = Pattern.compile("^=[A-Z][1-2]{0,1}[0-9][\\+|-][A-Z][1-2]{0,1}[0-9]$");
+					matcherOperazioni = patternOperazioni.matcher(text);
 					ritMatcherOperazione = matcherOperazioni.find();
 					System.out.println("Operazione: " + ritMatcherOperazione); //debug: stampa true se è un'operazione
 					
-					
-					Pattern patternNumeri = Pattern.compile("^-{0,1}[0-9]+$");
-					Matcher matcherNumeri = patternNumeri.matcher(text);
+					/** Controllo se la cella contiene solo numeri */
+					patternNumeri = Pattern.compile("^-{0,1}[0-9]+$");
+					matcherNumeri = patternNumeri.matcher(text);
 					ritMatcherNumeri = matcherNumeri.find();	
 					System.out.println("Numeri: " + ritMatcherNumeri); //debug: stampa true se sono numeri
 					
-					
-					if (ritMatcherNumeri == false && ritMatcherOperazione == false) //se entra è già testo e non è necessario effettuare controlli
+					/** Se il contenuto della cella non è nullo e non contiene formule o numeri, allora la cella contiene del testo */
+					if (ritMatcherNumeri == false && ritMatcherOperazione == false)
 					{	
 						ritMatcherTesto = true;
-						//Pattern patternTesto = Pattern.compile("^[A-Za-z0-9]+$"); 
-						//Matcher matcherTesto = patternTesto.matcher(text);
 						System.out.println("Testo : " + ritMatcherTesto); //debug: stampa true se è testo
 					}
 				}
 				
-				//assegna il valore nella struttura dati alla stringa temporanea text
+				/** Assegnamento del valore assegnato alla cella modificata nella struttura dati */
 				matrice[table.getSelectedRow()].get(table.getSelectedColumn()).setContCell(text);
 
-				
-				if (ritMatcherTesto == true) //conversione della cella a tipo CellaTesto
+				/** Conversione della cella a tipo <b>CellaTesto</b> */
+				if (ritMatcherTesto == true) 
 				{
-					//assegna il valore nella sottomatrice
+					/** Assegnamento del testo nella sottomatrice */
 					sottoMatrice.getDisplayer()[table.getSelectedRow()][table.getSelectedColumn()] = text;
 					
 					matrice[table.getSelectedRow()].set(table.getSelectedColumn(), new CellaTesto( 
 								matrice[table.getSelectedRow()].get(table.getSelectedColumn()).getContCell()));
 				}
 				
-				if(ritMatcherNumeri == true) //conversione della cella a tipo CellaNumeri
+				/** Conversione della cella a tipo <b>CellaNumeri</b> */
+				if(ritMatcherNumeri == true) 
 				{
 					if(sottoMatrice.getDisplayer()[table.getSelectedRow()][table.getSelectedColumn()].contains("=") == false)
 					{
-						//assegna il valore nella sottomatrice
+						/** Assegnamento del valore numerico nella sottomatrice */
 						sottoMatrice.getDisplayer()[table.getSelectedRow()][table.getSelectedColumn()] = text;
 					}
 					
@@ -210,21 +276,23 @@ public class Inizializzatore
 					
 				}
 				
-				if(ritMatcherOperazione == true) //conversione della cella a tipo CellaOperazione
+				/** Conversione della cella a tipo <b>CellaOperazione</b> */
+				if(ritMatcherOperazione == true) 
 				{
-					//assegna il valore nella sottomatrice
+					/** Assegnamento dell'operazione originaria nella sottomatrice */
 					sottoMatrice.getDisplayer()[table.getSelectedRow()][table.getSelectedColumn()] = text;
 					
-					//crea la nuova cella di tipo CellaOperazione
+					/** Creazione della nuova cella di tipo CellaOperazione */
 					matrice[table.getSelectedRow()].set(table.getSelectedColumn(),
 							new CellaOperazione(matrice, table, text));
 					
-					//assegna il dato di ritorno della CellaOperazione al DefaultTableModel
+					/** Assegnazione del dato di ritorno della CellaOperazione al DefaultTableModel */
 					dati.setValueAt(matrice[table.getSelectedRow()].get(table.getSelectedColumn()).toString(), table.getSelectedRow(), table.getSelectedColumn());
 					
 				}
 				
-				if(ritMatcherTesto == false && ritMatcherNumeri == false && ritMatcherOperazione == false) //se non è una cella di testo, o una cella numero o una cella operazione allora è una cella vuota e deve essere riconvertita al tipo CellaGenerica
+				/** Se la cella non contiene testo, o numeri, o formule allora si tratta di una cella vuota, e dovra' essere riconvertita al tipo <b>CellaGenerica</b> */
+				if(ritMatcherTesto == false && ritMatcherNumeri == false && ritMatcherOperazione == false) 
 				{
 					System.out.println("La cella di riga: " + table.getSelectedColumn() + " e colonna: " + table.getSelectedColumn() + " diventa una cella generica"); //debug
 					matrice[table.getSelectedRow()].set(table.getSelectedColumn(), new CellaGenerica());
@@ -233,23 +301,24 @@ public class Inizializzatore
 				System.out.println("Aggiorno il contenuto della cella di riga " + table.getSelectedRow() + " e colonna " + table.getSelectedColumn() + " a: " + text); //debug
 				
 				System.out.println("Contenuto struttura dati:"); //debug
-				for (int i = 0; i < dim; ++i)
-					System.out.println(matrice[i]);
+				for (int i = 0; i < dim; ++i) //debug
+					System.out.println(matrice[i]); //debug
 
+				/** Stampa del contenuto e del tipo della cella selezionata nel JTextField */
 				classeCellaSelezionata = matrice[table.getSelectedRow()].get(table.getSelectedColumn()).getClass() + "";
 				classeCellaSelezionata = "     \t\t\tTipo cella selezionato: " + classeCellaSelezionata.substring(6);
 				operationDisplayer.setText("Contenuto cella: " + sottoMatrice.getDisplayer()[table.getSelectedRow()][table.getSelectedColumn()] + classeCellaSelezionata);
 				
-				
+				/** Creazione di un Thread predisposto al salvataggio delle due strutture dati se non e' ancora stato creato in precedenza */
 				if (threadCreato == false)
 				{
 					threadCreato = true;
 					
-				    //variabile usata per ottenere il percorso assoluto corrente
 				    percorsoCorrente = new File("");
 				    
 				    Runnable salvataggioAutomatico = new Runnable() 
 				    {
+				    	/**	Metodo eseguito all'avvio del Thread */
 				        public void run() 
 				        {
 				            System.out.println("SALVATAGGIO AUTOMATICO"); //debug
@@ -257,15 +326,15 @@ public class Inizializzatore
 				    		
 				            try 
 				    		{
-				            	System.out.println("ENTRO TRY CATCH AUTOSAVE");
+				            	System.out.println("ENTRO TRY CATCH AUTOSAVE"); //debug
 				            	
-				            	//salvataggio la struttura dati principale su file binario con suffisso ".autosave"
+				            	/** Salvataggio la struttura dati principale su file binario con suffisso ".autosave" */
 				            	FileOutputStream fos = new FileOutputStream(percorsoCorrente.getAbsolutePath() + "\\.autosave");
 				    			ObjectOutputStream oos = new ObjectOutputStream(fos);
 				    			oos.writeObject(matrice);
 				    			oos.close();
 				            	
-				    			//salvataggio la sottomatrice su file binario con suffisso ".autosave.sottomatrice"
+				    			/** Salvataggio la sottomatrice su file binario con suffisso ".autosave.sottomatrice" */
 				            	FileOutputStream fos2 = new FileOutputStream(percorsoCorrente.getAbsolutePath() + "\\.autosave.sottomatrice");
 				    			ObjectOutputStream oos2 = new ObjectOutputStream(fos2);
 				    			oos2.writeObject(sottoMatrice);
@@ -281,75 +350,72 @@ public class Inizializzatore
 				    		}
 				        }
 				    };
-				    //crea un pool formato da un thread
+				    /** Creazione di un pool formato da un singolo Thread */
 				    ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-				    //abilita l'esecuzione di del salvataggio automatico dopo il ritardo iniziale di 0 secondi, a periodi regolari definiti dalla variabile timerAutosalvataggio
-				    executor.scheduleAtFixedRate(salvataggioAutomatico, 0, timerAutosalvataggio, TimeUnit.SECONDS);
+				    
+				    /** Abilitazione dell'esecuzione del salvataggio automatico dopo il ritardo iniziale di [<b>delaySalvataggio</b>] secondi, a periodi regolari definiti dalla variabile timerAutosalvataggio */
+				    executor.scheduleAtFixedRate(salvataggioAutomatico, delaySalvataggio, timerAutosalvataggio, TimeUnit.SECONDS);
 				}
 			}	
 		});
 		
-		//barra menu File
-		JMenuBar barraFile = new JMenuBar();
-        
-		//Crea il menu
-		JMenu menuFile = new JMenu("File");
-		
-        //Crea gli elementi del menu
-		JMenuItem opzioneMenuFile1 = new JMenuItem("Nuovo");
-		JMenuItem opzioneMenuFile2 = new JMenuItem("Apri");
-		JMenuItem opzioneMenuFile3 = new JMenuItem("Salva con nome");
-		JMenuItem opzioneMenuFile4 = new JMenuItem("Esci");
-		
+		/** Inizializzazione menu File */
+		barraFile = new JMenuBar();
+		menuFile = new JMenu("File");
+		opzioneMenuFile1 = new JMenuItem("Nuovo");
+		opzioneMenuFile2 = new JMenuItem("Apri");
+		opzioneMenuFile3 = new JMenuItem("Salva con nome");
+		opzioneMenuFile4 = new JMenuItem("Esci");
 		menuFile.add(opzioneMenuFile1);
         menuFile.add(opzioneMenuFile2);
         menuFile.add(opzioneMenuFile3);
         menuFile.add(opzioneMenuFile4);
         barraFile.add(menuFile);
 
-        //barra menu Help
-        JMenuBar barraHelp = new JMenuBar();
-        JMenu menuHelp = new JMenu("Aiuto");
-        JMenuItem opzioneMenuHelp1 = new JMenuItem("Documentazione");
-        JMenuItem opzioneMenuHelp2 = new JMenuItem("Traccia d'esame");
+        /** Inizializzazione menu Aiuto */
+        barraHelp = new JMenuBar();
+        menuHelp = new JMenu("Aiuto");
+        opzioneMenuHelp1 = new JMenuItem("Documentazione");
+        opzioneMenuHelp2 = new JMenuItem("Traccia d'esame");
         menuHelp.add(opzioneMenuHelp1);
         menuHelp.add(opzioneMenuHelp2);
         barraHelp.add(menuHelp);
         
-        JPanel pannelloNordUpper = new JPanel();
-        JPanel pannelloNord = new JPanel();
-        
-        FlowLayout leftAlignment = new FlowLayout();
+        /** Inizializzazione dei JPanel 
+         * 	pannelloNordUpper: contiene il menu
+         *  pannelloNord: contiene pannelloNordUpper e il JTextField */
+        pannelloNordUpper = new JPanel();
+        pannelloNord = new JPanel();
+        leftAlignment = new FlowLayout();
         leftAlignment.setAlignment(FlowLayout.LEFT);
-        
         pannelloNord.setLayout(new BorderLayout());
         pannelloNordUpper.setLayout(leftAlignment);
-        
         pannelloNordUpper.add(barraFile);
         pannelloNordUpper.add(barraHelp);
         pannelloNord.add(pannelloNordUpper, BorderLayout.NORTH);
         pannelloNord.add(operationDisplayer, BorderLayout.SOUTH);
 
+        /** Inizializzazione JTable e JScrollPane */
 		table.setCellSelectionEnabled(true);
 		table.setBackground(new Color(232, 255, 250));
 		table.setRowHeight(22);
 		
-		//setta la prima colonna con background grigio e testo allineato al centro e imposta la barra di scorrimento
-		JScrollPane sp = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	    DefaultTableCellRenderer setPrimaColonna = new DefaultTableCellRenderer();
+		/** Imposta la prima colonna con background grigio e testo allineato al centro e la barra di scorrimento */
+		sp = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		setPrimaColonna = new DefaultTableCellRenderer();
 	    setPrimaColonna.setBackground(new Color(238, 238, 238, 255));
 	    setPrimaColonna.setHorizontalAlignment(JLabel.CENTER);
 	    table.getColumnModel().getColumn(0).setCellRenderer(setPrimaColonna);
 	    table.getTableHeader().setReorderingAllowed(false);
-	    
 	    table.getColumnModel().getColumn(0).setMaxWidth(30);
 	    
-	    //Imposta la JTable affinché non ridimensioni automaticamente la dimensione delle colonne 
+	    /** Evita che la JTable non ridimensioni automaticamente la colonne al variare della finestra */
 	    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF); 
 	    
-	    Frame finestra = new Frame(pannelloNord, sp);
+	    finestra = new Frame(pannelloNord, sp);
 	    finestra.getF().setVisible(true);
 	    
+	    /** Creazione dell'oggetto incaricato dell'implementazione delle funzionalita' del menu */
 	    new Menu(dim, dati, table, finestra, 
 	    		opzioneMenuFile1, opzioneMenuFile2, opzioneMenuFile3, opzioneMenuFile4, 
 	    		opzioneMenuHelp1, opzioneMenuHelp2, 
